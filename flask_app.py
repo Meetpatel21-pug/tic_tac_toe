dd0da3a9305bbb95355249df7659e1c04b8aed78
Meet Parsana <meetparsana211@gmail.com>
2026-04-22 19:55:02 +0530

    Fix PythonAnywhere WSGI import path

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,11 @@
+import os
 import sys
-path = '/home/meet21/mysite'
-if path not in sys.path:
-    sys.path.append(path)
+
+PROJECT_HOME = "/home/meet21/mysite/tic_tac_toe"
+
+if PROJECT_HOME not in sys.path:
+    sys.path.insert(0, PROJECT_HOME)
+
+os.chdir(PROJECT_HOME)
 
 from app import app as application
